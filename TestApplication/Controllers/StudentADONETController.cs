2c86c163e9bb5db07db1cd752ce6acf59986367b
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-02-14 13:52:12 +0500

    add crud dapper and ado.net

---
+++ TestApplication/Controllers/StudentADONETController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestApplication.Models;
+using Npgsql;
+using System.Xml.Linq;
+namespace TestApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentADONETController : ControllerBase
+    {
+        private readonly string _conString = "Server=127.0.0.1;Port=5432;Database=TestDb;User Id=postgres;Password=root;";
+
+        [HttpGet]
+        [Route("ADO/Get")]
+        public List<Student> Get()
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_conString))
+            {
+                con.Open();
+                string query = "select * from students";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                List<Student> students = new List<Student>();
+                while (reader.Read())
+                {
+                    students.Add(new Student()
+                    {
+                        Id = (int)reader[0],
+                        Name = (string)reader[1],
+                        Age = (int)reader[2]
+                    });
+                }
+                return students;
+            }
+        }
+        [HttpPost]
+        [Route("ADO/Post")]
+        public int Post(string name, int age)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_conString))
+            {
+                con.Open();
+                string query = "insert into students(name,age) values(@name,@age);";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                cmd.Parameters.Add(new NpgsqlParameter("name", name));
+                cmd.Parameters.Add(new NpgsqlParameter("age", age));
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        [HttpDelete]
+        [Route("ADO/Delete")]
+        public int Delete(int id)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_conString))
+            {
+                con.Open();
+                string query = "delete from students where id = @id";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                cmd.Parameters.Add(new NpgsqlParameter("id", id));
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+
+        [HttpPut]
+        [Route("ADO/Put")]
+        public int Put(int id, string name, int age)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_conString))
+            {
+                con.Open();
+                string query = "update students set name = @name,age = @age where id = @id;";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                cmd.Parameters.Add(new NpgsqlParameter("id", id));
+                cmd.Parameters.Add(new NpgsqlParameter("name", name));
+                cmd.Parameters.Add(new NpgsqlParameter("age", age));
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        [HttpPatch]
+        [Route("ADO/Patch/age")]
+        public int Patch(int id, int age)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_conString))
+            {
+                con.Open();
+                string query = "update students set age = @age where id = @id;";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                cmd.Parameters.Add(new NpgsqlParameter("id", id));
+                cmd.Parameters.Add(new NpgsqlParameter("age", age));
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        [HttpPatch]
+        [Route("ADO/Patch/name")]
+        public int Patch(int id, string name)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_conString))
+            {
+                con.Open();
+                string query = "update students set name = @name where id = @id;";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                cmd.Parameters.Add(new NpgsqlParameter("id", id));
+                cmd.Parameters.Add(new NpgsqlParameter("name", name));
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ TestApplication/Controllers/StudentsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using TestApplication.Models;
+using Dapper;
+
+namespace TestApplication.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly string _conString = "Server=127.0.0.1;Port=5432;Database=TestDb;User Id=postgres;Password=root;";
+
+        [HttpGet]
+        public List<Student> GetStudents()
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_conString))
+            {
+                string query = "select * from students";
+                return con.Query<Student>(query).ToList();
+            }
+        }
+
+        [HttpPost]
+        public int AddStudent(string studentName, int studentAge)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_conString))
+            {
+                string query = "insert into students(name,age) values(@name,@age);";
+                return con.Execute(query, new { name = studentName, age = studentAge });
+            }
+        }
+
+        [HttpDelete]
+        public int DeleteStudent(int studentId)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_conString))
+            {
+                string query = "delete from students where id = @delId";
+                return con.Execute(query, new { delId = studentId });
+            }
+        }
+
+        [HttpPatch]
+        public int PatchStudentName(int studentId, string studentName)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_conString))
+            {
+                string query = "update students set name = @names where id = @ids";
+                return con.Execute(query, new { ids = studentId, names = studentName });
+            }
+        }
+
+        [HttpPatch]
+        public int PatchStudentAge(int studentId, int studentAge)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_conString))
+            {
+                string query = "update students set age = @age where id = @ids";
+                return con.Execute(query, new { age = studentAge, ids = studentId });
+            }
+        }
+
+        [HttpPut]
+        public int PutStudentAge(int studentId, string studentName, int studentAge)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(_conString))
+            {
+                string query = "update students set name = @name, age = @age where id = @ids";
+                return con.Execute(query, new { age = studentAge, name = studentName, ids = studentId });
+            }
+        }
+
+    }
+}
